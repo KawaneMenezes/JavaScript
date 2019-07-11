414949d845d54abf6e71ed75fe6f8506f52602a6
KawaneMenezes <kaw.menezes@gmail.com>
2019-07-11 10:25:02 -0300

    consultar projetos

---
+++ script.js
@@ -0,0 +1,17 @@
+alert ('Olá');
+
+let Aluno = prompt ('Qual o  nome do aluno?');
+let qualamatéria = prompt ('Qual o nome da  matéria ?');
+let qualanota = prompt('Qual foi a nota ?');
+
+let media = 7;
+
+
+if (media >= 7){
+alert ('Parabéns, você está aprovado!');
+}
+else if(media>=5.5 || media <= 6.9){
+    alert ("Poxa, infelizmente você está de recuperação.");
+}
+
+
